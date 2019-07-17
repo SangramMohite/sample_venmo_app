ac5818362a2f0dde9829ae7c1f03f0e38cb60c3f
sangram.github <sangram.github@gmail.com>
2019-07-17 00:45:26 -0700

    Create, show Account and balance

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 group :development, :test do
   gem 'pg', '>= 0.18', '< 2.0'
+  gem 'rspec-rails', '~> 3.8'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,19 +1,51 @@
 class AccountsController < ApplicationController
 
 	def index
-		 
+		@user = get_user		
+		if @user
+			@account = Account.find_by(user_id: @user.id)
+			if @account
+				render json: @account
+			else
+				render json: {"error": "No account exists for user #{@user.name}"}
+			end
+		else
+			render json: {"error": "user does not exist"}
+		end		 
 	end
 
-	def show
+	def show		
+	end
+
+	def create
+		@user = User.find_by(id: account_params[:user_id])
+		if @user
+			if !Account.find_by(user_id: @user.id)			
+				@account = @user.build_account(name: params[:name], balance:  params[:balance]) 
+				if @account.save
+				# saved account to db is successful
+	  				render json: @account
+	  			else
+			  		# render 'new' which will give users the error messages of why saving failed.
+			  		render json: @account.errors.full_messages
+			  	end
+			  else
+			  	# redirect user to settings page or delete account page with their account loaded.
+			  		render json: {"error": "Account for #{@user.name} already exists. Only one account per user."}
+			  	end
+		else
+			render json: {"error": "User does not exist"}
+		end		
 	end
 
 	def balance
-		user_id = params[:user_id]
-		user = User.find_by(id: user_id)
-		if user
-			account = Account.find_by(user_id: user_id)
-			if account
-				render json: user.account.balance
+		# user_id = user_params
+		# @user = User.find_by(id: user_id)
+		@user = get_user
+		if @user
+			@account = Account.find_by(user_id: @user.id)
+			if @account
+				render json: @account.balance
 			else
 				render json: {"error": "No account exists for user"}
 			end	
@@ -22,4 +54,19 @@ class AccountsController < ApplicationController
 		end
 		
 	end
+
+	  private
+
+  def user_params
+  	params.require(:user_id)
+  end
+
+  def account_params
+  	params.require(:account).permit(:user_id, :name, :balance)
+  end
+
+  def get_user
+  	User.find_by(id: user_params)
+  end
+
 end
